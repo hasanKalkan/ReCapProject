19e309a13f014d68fee00e37d15f79252d39d2ed
krack16 <47774247+krack16@users.noreply.github.com>
2021-02-03 09:23:51 +0300

    3221

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
 
 namespace ConsoleUI
 {
@@ -6,7 +8,13 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-           
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("{0}", car.Description);
+            }
+
+            
         }
     }
 }
