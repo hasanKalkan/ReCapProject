2f6188bb764b32765e44bac56f835179fa0091d7
krack16 <47774247+krack16@users.noreply.github.com>
2021-02-04 16:06:25 +0300

    4221

---
+++ DataAccess/Concrete/EntityFramework/ReCapProjectContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class ReCapProjectContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ReCapProject;Trusted_Connection=true"); // @\\ @ işareti slaş işaretini ters slaş olarak ayarlar.
+            //veri tabanı isminde büyük küçük harf farketmez.
+            //trusted ile integrated security aynı
+        }
+
+        public DbSet<Car> Products { get; set; }
+        public DbSet<Color> Categories { get; set; }
+        public DbSet<Brand> Customers { get; set; }
+    }
+}
